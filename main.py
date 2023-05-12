8ea90753ab26dff530692503b3759fe8dbc3ba65
Nagidas <122519870+Nagidas@users.noreply.github.com>
2023-05-12 03:32:45 +0300

    Affichage des elements de la liste

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ if __name__ == '__main__':
     # Liste de 10 elements de type chaîne de caractères
     List = ["papa", "maman", "fils", "fille", "oncle", "tante", "neveu", "niece", "parent", "enfant"]
     
+# 1- Affichage des elements de la liste
+    print("1) Affichage des elements de la liste")
+    print(List)
+    print("\n")
